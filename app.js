8669bc14d1d4bb4462622c9fcbfe21194afed666
Edy M. Sotomayor Juarez <edysotomayor@Edys-MacBook-Air.local>
2022-06-06 21:55:16 -0300

    first comit

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get('/register', (req,res)=>{
+  res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+  res.sendFile(__dirname + '/views/login.html');
+});
+
+
+app.listen(3000, ()=> {
+  console.log('Servidor Corriendo en puerto 3000');
+});
